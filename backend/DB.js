fdc1b7f9293a9ab74fc1963c9d5c77bcb2e8baf1
Veer Khatri <veerkhatri70@gmail.com>
2021-09-07 12:44:14 +0530

    express server setup

---
+++ backend/DB.js
@@ -0,0 +1,11 @@
+// DataBase File
+const mongoose = require("mongoose");
+const mongoURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
+
+const connectToMongo = async () =>{
+    mongoose.connect(mongoURI, ()=>{
+        console.log("connected to mongo successfully")
+    })
+}
+
+module.exports = connectToMongo;
--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,14 @@
 // entry point of backend
+const express = require('express')
+const connectToMongo = require('./DB')
+connectToMongo();
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello veeru!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
