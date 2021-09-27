a533606d3079be7cc82b29da94c96e1809be126d
Veer Khatri <veerkhatri70@gmail.com>
2021-09-27 12:32:06 +0530

    updating notes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const fetchuser = require("../middleware/fetchUser");
 const Notes = require("../mongoose models/Notes")
 const { body, validationResult } = require('express-validator');
+const { request } = require("express");
 
 
 
@@ -18,13 +19,13 @@ router.get("/fetchnotes", fetchuser, async (requestt, responsee) => {
     }
 })
 
-// Appending/Adding notes using: POST "/api/notes/appendnotes" login required
-router.post("/appendnotes", fetchuser, [
+// Route 2- Appending/Adding notes using: POST "/api/notes/addnotes" login required
+router.post("/addnotes", fetchuser, [
     body('title', "Title can't be empty enter at lease one letter").isLength({ min: 1 }),
     body('description', "Description must contain 10 characters or more").isLength({ min: 10 })
 ], async (requestt, responsee) => {
     try {
-        
+
         const errors = validationResult(requestt);
         const { title, description, tag } = requestt.body;
         // if there are errors return bad request and the errors
@@ -46,4 +47,50 @@ router.post("/appendnotes", fetchuser, [
     }
 })
 
+// Route 3- updatting an existing note using: PUT "/api/notes/updatenote" login required
+
+router.put("/updatenote/:id", fetchuser, [
+    body('title', "Title can't be empty enter at lease one letter").isLength({ min: 1 }),
+    body('description', "Description must contain 10 characters or more").isLength({ min: 10 })
+], async (requestt, responsee) => {
+    try {
+
+        const errors = validationResult(requestt);
+        const { title, description, tag } = requestt.body;
+        if (!errors.isEmpty()) {
+            return responsee.status(400).json({ errors: errors.array() });
+        }
+        // Creating new note object
+        const newNote = {}
+        // added  all the thing in newNote object
+        if (title) {
+            newNote.title = title
+        }
+        if (description) {
+            newNote.description = description
+        }
+        if (tag) {
+            newNote.tag = tag
+        }
+        // find the note to be updated
+        let note = await Notes.findById(requestt.params.id);// gave id in api so we can have the id of note 
+        console.log(newNote);
+        if (!note) {
+            //if note not present
+            return responsee.status(404).send("note not found")
+        }
+
+        // checking the user to be the same
+        if (note.user.toString() !== requestt.user.id) {
+            return responsee.status(401).send("Want to work for me Hacker?")
+
+        }
+        // checked note is there or not and user is authorised to do it or not For Stopping hackers
+        note = await Notes.findByIdAndUpdate(requestt.params.id, { $set: newNote }, { new: true })
+        responsee.json({ note });
+    } catch (error) {
+        console.error(error.message);
+        responsee.status(500).send("internal server error")
+    }
+})
 module.exports = router
